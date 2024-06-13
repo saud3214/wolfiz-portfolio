a85251182d083477eac8df959e1ff1106005c294
saud3214 <abbasisaud8@gmail.com>
2024-06-13 18:00:06 +0500

    v3

--- app/(portfolio)/webprojects/page.tsx
+++ app/(portfolio)/webprojects/page.tsx
@@ -26,13 +26,13 @@ export default function Webprojects() {
           speed={1000}
           className=" w-full h-full items-center p-2 slideclas "
         >
-          <SwiperSlide className=" border border-red-400 slideclas bg-lime-200  ">
+          <SwiperSlide className=" border border-red-400 slideclas bg-lime-200  text-2xl font-bold">
             Slide 1
           </SwiperSlide>
-          <SwiperSlide className=" border border-red-400 slideclas  bg-green-200">
+          <SwiperSlide className=" border border-red-400 slideclas  bg-green-200 text-2xl font-bold">
             Slide 2
           </SwiperSlide>
-          <SwiperSlide className=" border border-red-400 slideclas bg-pink-200">
+          <SwiperSlide className=" border border-red-400 slideclas bg-pink-200 text-2xl font-bold">
             Slide 3
           </SwiperSlide>
         </Swiper>
