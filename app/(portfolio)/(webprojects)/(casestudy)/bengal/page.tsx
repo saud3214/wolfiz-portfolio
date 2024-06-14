20e143fd0c47382bf49da370a22aed14bf25a909
saud3214 <abbasisaud8@gmail.com>
2024-06-14 10:16:21 +0500

    v4

---
+++ app/(portfolio)/(webprojects)/(casestudy)/bengal/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Image from 'next/image';
+import { gsap } from 'gsap';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect } from 'react';
+
+export default function Bengal() {
+  return (
+    <div className="  h-full w-full flex flex-col items-center justify-center p-6  ">
+      <div className="h-full w-full items-center justify-center flex bg-red-400">
+        Bengal Case Study
+      </div>
+    </div>
+  );
+}
