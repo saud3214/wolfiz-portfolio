a4381a3fb00f63cff2454122ba4bcc4c2b0e9721
saud3214 <abbasisaud8@gmail.com>
2024-08-21 17:55:05 +0500

    v8.21

--- app/(portfolio)/(mobileapps)/(casestudy)/ndusapp/page.tsx
+++ app/(portfolio)/(mobileapps)/(casestudy)/ndusapp/page.tsx
@@ -53,24 +53,28 @@ export default function Ndusapp() {
               <div className="flex flex-col w-full items-center justify-center ">
                 <div className="grid grid-cols-12 w-full items-center justify-center  h-[60vh]">
                   <div className=" col-span-6 flex text-start bg-ndubg1 bg-cover bg-center bg-no-repeat p-10 h-full items-center justify-start">
-                    <span className="text-white 2xl:text-5xl text-4xl font-bold uppercase 2xl:leading-[60px]">
-                      national<br></br> defense<br></br> university <br></br>
-                      scheduler app
-                    </span>
+                    <SrBottom>
+                      <span className="text-white 2xl:text-5xl text-4xl font-bold uppercase 2xl:leading-[60px]">
+                        national<br></br> defense<br></br> university <br></br>
+                        scheduler app
+                      </span>
+                    </SrBottom>
                   </div>
                   <div className=" col-span-6 flex  flex-col text-start bg-ndubg2 bg-cover bg-center bg-no-repeat p-10 h-full items-start justify-center">
                     <span className="text-white text-3xl font-semibold">
                       PROBLEM STATMENT:
                     </span>
-                    <span className="text-white 2xl:text-2xl text-xl font-medium">
-                      Universities often face inefficiencies with outdated
-                      scheduling systems that lead to conflicts and
-                      administrative burdens. The goal is to create an intuitive
-                      app that minimizes scheduling conflicts,adapts to changes,
-                      integrates with existing systems, optimizes resource use,
-                      and provides useful analytics, improving overall
-                      efficiency and user satisfaction
-                    </span>
+                    <RevealOnScroll>
+                      <span className="text-white 2xl:text-2xl text-xl font-medium">
+                        Universities often face inefficiencies with outdated
+                        scheduling systems that lead to conflicts and
+                        administrative burdens. The goal is to create an
+                        intuitive app that minimizes scheduling conflicts,adapts
+                        to changes, integrates with existing systems, optimizes
+                        resource use, and provides useful analytics, improving
+                        overall efficiency and user satisfaction
+                      </span>
+                    </RevealOnScroll>
                   </div>
                 </div>
                 <div className="grid grid-cols-12 w-full items-center justify-center  h-[60vh]">
@@ -78,34 +82,42 @@ export default function Ndusapp() {
                     <span className="text-white text-3xl font-semibold uppercase">
                       solution statement:
                     </span>
-                    <span className="text-white 2xl:text-2xl text-xl font-medium">
-                      We propose an advanced scheduling app that simplifies and
-                      optimizes course and resource management. It will feature
-                      an intuitive interface, real-time conflict resolution,
-                      seamless integration with existing systems, and analytics
-                      tools to enhance decision-making and maximize efficiency.
-                    </span>
+                    <RevealOnScroll>
+                      <span className="text-white 2xl:text-2xl text-xl font-medium">
+                        We propose an advanced scheduling app that simplifies
+                        and optimizes course and resource management. It will
+                        feature an intuitive interface, real-time conflict
+                        resolution, seamless integration with existing systems,
+                        and analytics tools to enhance decision-making and
+                        maximize efficiency.
+                      </span>
+                    </RevealOnScroll>
                   </div>
                   <div className=" col-span-6 flex items-center justify-center  bg-ndubg6 bg-cover bg-center bg-no-repeat  w-full h-full   "></div>
                 </div>
               </div>
 
               <div className="w-full flex flex-col items-start justify-start bg-ndubg4 bg-center bg-cover bg-no-repeat  p-10">
-                <span className="bg-gradient-to-r from-[#82D674] to-[#205532] bg-clip-text text-transparent 2xl:text-[5vw] text-[3vw] font-bold uppercase">
-                  colors & typography
-                </span>
-
-                <span className="text-white 2xl:text-[5vw] text-[3vw] font-bold uppercase 2xl:leading-[90px] mt-5">
-                  Inter <br></br> helvatica
-                </span>
+                <RevealOnScroll>
+                  <span className="bg-gradient-to-r from-[#82D674] to-[#205532] bg-clip-text text-transparent 2xl:text-[5vw] text-[3vw] font-bold uppercase">
+                    colors & typography
+                  </span>
+                </RevealOnScroll>
+                <SrBottom>
+                  <span className="text-white 2xl:text-[5vw] text-[3vw] font-bold uppercase 2xl:leading-[90px] mt-5">
+                    Inter <br></br> helvatica
+                  </span>
+                </SrBottom>
                 <div className="w-full items-center justify-center flex flex-col pt-10">
                   <div className="flex  items-center  relative   w-2/5    ">
-                    <Image
-                      className=" relativepos  "
-                      src="/mobileapps/ndusapp/cap.png"
-                      alt="Background 1"
-                      fill
-                    />
+                    <SrBottom>
+                      <Image
+                        className=" relativepos  "
+                        src="/mobileapps/ndusapp/cap.png"
+                        alt="Background 1"
+                        fill
+                      />
+                    </SrBottom>
                   </div>
                   <div className="w-full flex items-end justify-end text-white 2xl:text-4xl text-2xl font-bold px-5 py-10">
                     <span>
@@ -113,12 +125,14 @@ export default function Ndusapp() {
                       1234567890!@#$%^&*()_+-=
                     </span>
                     <div className="flex  items-center  absolute  left-[10%]  w-1/4  ">
-                      <Image
-                        className=" relativepos  "
-                        src="/mobileapps/ndusapp/aa.png"
-                        alt="Background 1"
-                        fill
-                      />
+                      <SrLeft>
+                        <Image
+                          className=" relativepos  "
+                          src="/mobileapps/ndusapp/aa.png"
+                          alt="Background 1"
+                          fill
+                        />
+                      </SrLeft>
                     </div>
                   </div>
                 </div>
@@ -126,121 +140,147 @@ export default function Ndusapp() {
 
               <div className="w-10/12 items-center justify-center py-20">
                 <div className="flex  items-center w-full  ">
-                  <Image
-                    className=" relativepos  "
-                    src="/mobileapps/ndusapp/color.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <SrBottom>
+                    <Image
+                      className=" relativepos  "
+                      src="/mobileapps/ndusapp/color.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </SrBottom>
                 </div>
               </div>
               <div className="w-full items-center justify-center flex bg-ndubg5 bg-cover bg-center bg-no-repeat">
                 <div className="flex  items-center w-10/12   ">
-                  <Image
-                    className=" relativepos  "
-                    src="/mobileapps/ndusapp/laptop.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <SrBottom>
+                    <Image
+                      className=" relativepos  "
+                      src="/mobileapps/ndusapp/laptop.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </SrBottom>
                 </div>
               </div>
 
               <div className="w-full bg-white flex items-center justify-center p-10">
                 <div className="w-11/12 flex flex-col items-center justify-center">
                   <span className="uppercase  text-8xl font-bold text-[#072507]">
-                    design process
+                    <AnimatedText2 text="design process"></AnimatedText2>
                   </span>
 
                   <div className="w-full items-center justify-between flex">
                     <div className="flex  items-center w-1/2    ">
-                      <Image
-                        className=" relativepos  "
-                        src="/mobileapps/ndusapp/dp1.png"
-                        alt="Background 1"
-                        fill
-                      />
+                      <SrLeft>
+                        <Image
+                          className=" relativepos  "
+                          src="/mobileapps/ndusapp/dp1.png"
+                          alt="Background 1"
+                          fill
+                        />
+                      </SrLeft>
                     </div>
                     <div className="flex  items-center   w-1/2  ">
-                      <Image
-                        className=" relativepos  "
-                        src="/mobileapps/ndusapp/dp2.png"
-                        alt="Background 1"
-                        fill
-                      />
+                      <SrRight>
+                        <Image
+                          className=" relativepos  "
+                          src="/mobileapps/ndusapp/dp2.png"
+                          alt="Background 1"
+                          fill
+                        />
+                      </SrRight>
                     </div>
                   </div>
                   <div className="w-full items-center justify-between flex">
                     <div className="flex  items-center w-1/2    ">
-                      <Image
-                        className=" relativepos  "
-                        src="/mobileapps/ndusapp/dp3.png"
-                        alt="Background 1"
-                        fill
-                      />
+                      <SrLeft>
+                        <Image
+                          className=" relativepos  "
+                          src="/mobileapps/ndusapp/dp3.png"
+                          alt="Background 1"
+                          fill
+                        />
+                      </SrLeft>
                     </div>
                     <div className="flex  items-center   w-1/2  ">
-                      <Image
-                        className=" relativepos  "
-                        src="/mobileapps/ndusapp/dp4.png"
-                        alt="Background 1"
-                        fill
-                      />
+                      <SrRight>
+                        <Image
+                          className=" relativepos  "
+                          src="/mobileapps/ndusapp/dp4.png"
+                          alt="Background 1"
+                          fill
+                        />
+                      </SrRight>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="w-full flex   ">
                 <div className="flex  items-center   w-1/2   ">
-                  <Image
-                    className=" relativepos  "
-                    src="/mobileapps/ndusapp/mu1.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <SrLeft>
+                    <Image
+                      className=" relativepos  "
+                      src="/mobileapps/ndusapp/mu1.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </SrLeft>
                 </div>
                 <div className="flex  items-center   w-1/2  ">
-                  <Image
-                    className=" relativepos  "
-                    src="/mobileapps/ndusapp/mu2.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <SrRight>
+                    <Image
+                      className=" relativepos  "
+                      src="/mobileapps/ndusapp/mu2.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </SrRight>
                 </div>
               </div>
               <div className="w-full items-center justify-center flex flex-col bg-gradient-to-r from-[#00582F] to-[#00BE65] p-20">
                 <span className=" 2xl:text-[5vw] text-[3vw] font-bold uppercase py-10 text-[#82D674]">
-                  information architecture
+                  <AnimatedText2 text="information architecture"></AnimatedText2>
                 </span>
                 <div className="flex  items-center w-11/12 pb-10   ">
-                  <Image
-                    className=" relativepos    "
-                    src="/mobileapps/ndusapp/ia.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <RevealOnScroll>
+                    <Image
+                      className=" relativepos    "
+                      src="/mobileapps/ndusapp/ia.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </RevealOnScroll>
                 </div>
               </div>
               <div className="w-full items-center justify-center flex p-10">
                 <div className="flex  items-center  w-11/12  ">
-                  <Image
-                    className=" relativepos    "
-                    src="/mobileapps/ndusapp/mu3.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <SrLeft>
+                    <Image
+                      className=" relativepos    "
+                      src="/mobileapps/ndusapp/mu3.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </SrLeft>
                 </div>
               </div>
               <div className="w-full items-center justify-center flex flex-col bg-gradient-to-b from-[#000000] to-[#023019] ">
-                <span className="bg-gradient-to-r from-[#82D674] to-[#205532] bg-clip-text text-transparent 2xl:text-[5vw] text-[3vw] font-bold uppercase py-20">
-                  app screen
-                </span>
+                <RevealOnScroll>
+                  <div className="flex w-full items-center justify-center">
+                    <span className="bg-gradient-to-r from-[#82D674] to-[#205532] bg-clip-text text-transparent 2xl:text-[5vw] text-[3vw] font-bold uppercase py-20">
+                      app screen
+                    </span>
+                  </div>
+                </RevealOnScroll>
                 <div className="flex  items-center w-full pb-20   ">
-                  <Image
-                    className=" relativepos    "
-                    src="/mobileapps/ndusapp/screen1.png"
-                    alt="Background 1"
-                    fill
-                  />
+                  <SrBottom>
+                    <Image
+                      className=" relativepos    "
+                      src="/mobileapps/ndusapp/screen1.png"
+                      alt="Background 1"
+                      fill
+                    />
+                  </SrBottom>
                 </div>
               </div>
 
